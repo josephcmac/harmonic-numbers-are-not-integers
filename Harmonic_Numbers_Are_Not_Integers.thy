13f34f48c40916f6821d051acab42bb210246c41
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2020-02-12 00:20:25 +0200

    update

--- Harmonic_Numbers_Are_Not_Integers.thy
+++ Harmonic_Numbers_Are_Not_Integers.thy
@@ -12,19 +12,42 @@ begin
 
 text \<open>
  In 1915, L. Theisinger ~\cite{theisinger1915bemerkung} proved that, except for the trivial 
- case of 1, the harmonic numbers are not integers. We formalize this result as theorem 
- @{text harmonic_numbers_are_not_integers}, which is deduced from the computation of the 
- 2-adic norm of harmonic numbers (lemma @{text harmonic_numbers_2norm}).
+ case of 1, the harmonic numbers are not integers. In 1918, 
+ J. K{\"u}rsch{\'a}k  ~\cite{kurschak1918harmonic} provided a sufficient condition for the 
+ difference between two harmonic numbers not to be an integer. We formalize these result as theorems
+ @{text Taeisinger} and @{text Kurschak}, respectively. These results will be deduced from the 
+ computation of the 2-adic norm of harmonic numbers (lemma @{text harmonic_numbers_2norm}).
 \<close>
 
+subsection \<open>Main definition\<close>
+
+
+text \<open>
+ We start by defining the harmonic numbers.
+\<close>
+
+fun harmonic :: \<open>nat \<Rightarrow> rat\<close> where
+  \<open>harmonic 0 = 0\<close> |
+  \<open>harmonic (Suc n) = harmonic n + Fract 1 (Suc n)\<close>
+
+lemma harmonic_explicit:
+  \<open>harmonic n = (\<Sum>k = 1..n. (Fract 1 (of_nat k)) )\<close>
+proof(induction n)
+  case 0
+  thus ?case
+    by simp 
+next
+  case (Suc n)
+  thus ?case
+    by simp 
+qed
 
 subsection \<open>Auxiliary result\<close>
 
 lemma sum_last:
   fixes n::nat and a::\<open>nat \<Rightarrow> real\<close>
   assumes \<open>n \<ge> 2\<close>
-  shows \<open>(\<Sum>k = 1..n - 1. (a k)) + (a n) =
-    (\<Sum>k = 1..n. (a k))\<close>
+  shows \<open>(\<Sum>k = 1..n - 1. (a k)) + (a n) = (\<Sum>k = 1..n. (a k))\<close>
   using \<open>n \<ge> 2\<close>
   apply auto
   by (smt Suc_leD le_add_diff_inverse numeral_1_eq_Suc_0 numeral_2_eq_2 numeral_One plus_1_eq_Suc sum.nat_ivl_Suc')
@@ -32,7 +55,7 @@ lemma sum_last:
 lemma harmonic_numbers_2norm:
   fixes n :: nat
   assumes \<open>n \<ge> 2\<close>
-  shows \<open>pnorm 2 (\<Sum>k = 1..n. (Fract 1 (of_nat k))) = 2^(nat(\<lfloor>log 2 n\<rfloor>))\<close>
+  shows \<open>pnorm 2 (harmonic n) = 2^(nat(\<lfloor>log 2 n\<rfloor>))\<close>
 proof-
   define l where \<open>l = nat(\<lfloor>log 2 n\<rfloor>)\<close>
   define H where \<open>H = (\<Sum>k = 1..n. (Fract 1 (of_nat k)))\<close>
@@ -358,7 +381,210 @@ proof-
       using pnorm_unit_ball[where p = 2 and x = "(2^l) *  (Fract 1 (of_nat (2^l)))" and y = "(2^l) * pre_H"]
       by simp
     moreover have \<open>pnorm 2 ((2^l) * post_H) < 1\<close>
-      sorry
+    proof(cases \<open>2^l + 1 \<le> n\<close>)
+      case True
+      have \<open>pnorm 2 ((2^l) * post_H) = pnorm 2 (\<Sum>k = 2 ^ l + 1..n.  (2 ^ l)*(Fract 1 k))\<close>
+      proof-
+        have \<open>(2^l) * post_H = (\<Sum>k = 2 ^ l+1..n. (2 ^ l)*(Fract 1 k))\<close>
+          unfolding post_H_def
+          using Groups_Big.semiring_0_class.sum_distrib_left[where r = \<open>2^l\<close> 
+              and f = \<open>(\<lambda> k. Fract 1 k)\<close> and A = \<open>{2 ^ l+1..n}\<close>]
+          by auto
+        thus ?thesis
+          by simp
+      qed
+      also have \<open>\<dots>
+           = pnorm 2 (sum (\<lambda> k. (2 ^ l)*(Fract 1 k)) {2 ^ l + 1..n})\<close>
+        by blast
+      also have \<open>\<dots>
+           \<le> Max ((\<lambda> k. pnorm 2 ((2 ^ l)*(Fract 1 k))) ` {2 ^ l + 1..n})\<close>
+      proof-
+        have \<open>finite {2 ^ l + 1..n}\<close>
+          by simp          
+        moreover have \<open>{2 ^ l + 1..n} \<noteq> {}\<close>
+          using True 
+          by auto          
+        ultimately show ?thesis 
+          using \<open>prime 2\<close>  pnorm_ultratriangular_sum[where p = 2 and A = \<open>{2 ^ l + 1..n}\<close> 
+              and x = \<open>(\<lambda> k. (2 ^ l)*(Fract 1 k))\<close>]
+          by auto
+      qed
+      finally have \<open>pnorm 2 ((2^l) * post_H) \<le> 
+          Max ((\<lambda> k. pnorm 2 ((2 ^ l)*(Fract 1 k))) ` {2 ^ l + 1..n})\<close>
+        using \<open>pnorm 2 (2 ^ l * post_H) = pnorm 2 (\<Sum>k = 2 ^ l + 1..n. 2 ^ l * Fract 1 (int k))\<close> \<open>pnorm 2 (\<Sum>k = 2 ^ l + 1..n. 2 ^ l * Fract 1 (int k)) \<le> (MAX k\<in>{2 ^ l + 1..n}. pnorm 2 (2 ^ l * Fract 1 (int k)))\<close> 
+        by linarith
+      moreover have \<open>((\<lambda> k. pnorm 2 ((2 ^ l)*(Fract 1 k))) ` {2 ^ l + 1..n}) \<noteq> {}\<close>
+        using True 
+        by auto        
+      moreover have \<open>finite ((\<lambda> k. pnorm 2 ((2 ^ l)*(Fract 1 k))) ` {2 ^ l + 1..n})\<close>
+        by blast        
+      moreover have \<open>x \<in> (\<lambda> k. pnorm 2 ((2 ^ l)*(Fract 1 k))) ` {2 ^ l + 1..n} \<Longrightarrow> x < 1\<close>
+        for x
+      proof-
+        assume \<open>x \<in> (\<lambda> k. pnorm 2 ((2 ^ l)*(Fract 1 k))) ` {2 ^ l + 1..n}\<close>
+        then obtain t where \<open>t \<in> {2 ^ l + 1..n}\<close> and \<open>x = pnorm 2 ((2 ^ l)*(Fract 1 t))\<close>
+          by auto
+        have  \<open>x = (pnorm 2 (2 ^ l)) * (pnorm 2 (Fract 1 t))\<close>
+          using \<open>prime 2\<close> \<open>x = pnorm 2 ((2 ^ l)*(Fract 1 t))\<close> pnorm_multiplicativity
+          by auto
+        moreover have \<open>pnorm 2 (2 ^ l) = 1/(2^l)\<close>
+          using \<open>prime 2\<close> pval_primepow[where p = "2::nat"]
+          by (metis of_int_numeral of_nat_numeral pnorm_primepow)          
+        moreover have \<open>pnorm 2 (Fract 1 t) < 2^l\<close>
+        proof(rule classical)
+          assume \<open>\<not> (pnorm 2 (Fract 1 t) < 2^l)\<close>
+          hence \<open>pnorm 2 (Fract 1 t) \<ge> 2^l\<close>
+            by auto
+          moreover have \<open>2 powr l = 2^l\<close>
+            using powr_realpow 
+            by auto            
+          ultimately have \<open>pnorm 2 (Fract 1 t) \<ge> 2 powr l\<close>
+            by auto
+          moreover have \<open>pnorm 2 (Fract 1 t) = 2 powr (-pval 2 (Fract 1 t))\<close>
+          proof-
+            have \<open>t \<noteq> 0\<close>
+              using \<open>t \<in> {2^l + 1 .. n}\<close>
+              by simp
+            hence \<open>Fract 1 t \<noteq> 0\<close>
+            proof -
+              have "\<not> int t \<le> 0"
+                by (metis \<open>t \<noteq> 0\<close> of_nat_le_0_iff)
+              hence "\<not> Fract 1 (int t) \<le> 0"
+                by (simp add: Fract_le_zero_iff)
+              thus ?thesis
+                by linarith
+            qed              
+            thus ?thesis 
+              using pnorm_simplified
+              by simp
+          qed
+          ultimately have \<open>-pval 2 (Fract 1 t) \<ge> l\<close>
+            by simp            
+          hence \<open>-(multiplicity 2 (fst (quotient_of (Fract 1 t))))
+               + (multiplicity 2 (snd (quotient_of (Fract 1 t))))
+                     \<ge> l\<close>
+            unfolding pval_def 
+            by auto
+          have \<open>quotient_of (Fract (1::int) t) = (1, t)\<close>
+          proof-
+            have \<open>coprime (1::int) t\<close>
+              by simp              
+            moreover have \<open>t > 0\<close>
+              using \<open>t \<in> {2^l + 1 .. n}\<close>
+              by simp
+            ultimately show ?thesis
+              by (simp add: quotient_of_Fract)              
+          qed
+          hence \<open>fst (quotient_of (Fract 1 t)) = 1\<close>
+            by simp
+          moreover have \<open>snd (quotient_of (Fract 1 t)) = t\<close>
+            using \<open>quotient_of (Fract (1::int) t) = (1, t)\<close>
+            by auto
+          ultimately have \<open>- int(multiplicity (2::int) 1) + int(multiplicity (2::int) t) \<ge> l\<close>
+            using \<open>-(multiplicity 2 (fst (quotient_of (Fract 1 t))))
+               + (multiplicity 2 (snd (quotient_of (Fract 1 t))))
+                     \<ge> l\<close>
+            by auto
+          moreover have \<open>multiplicity (2::int) 1 = 0\<close>
+            by simp
+          ultimately have \<open>multiplicity (2::int) t \<ge> l\<close>
+            by auto
+          hence \<open>2^l dvd t\<close>
+            by (metis int_dvd_int_iff multiplicity_dvd' of_nat_numeral of_nat_power)
+          hence \<open>\<exists> k::nat. 2^l * k = t\<close>
+            by auto
+          then obtain k::nat where \<open>2^l * k = t\<close>
+            by blast
+          have \<open>k \<ge> 2\<close>
+          proof(rule classical)
+            assume \<open>\<not>(k \<ge> 2)\<close>
+            hence \<open>k < 2\<close>
+              by simp
+            moreover have \<open>k \<noteq> 0\<close>
+            proof(rule classical)
+              assume \<open>\<not>(k \<noteq> 0)\<close>
+              hence \<open>k = 0\<close>
+                by simp
+              hence \<open>t = 0\<close>
+                using \<open>2^l * k = t\<close>
+                by auto
+              thus ?thesis
+                using \<open>t \<in> {2^l + 1 .. n}\<close>
+                by auto
+            qed
+            moreover have \<open>k \<noteq> 1\<close>
+            proof(rule classical)
+              assume \<open>\<not>(k \<noteq> 1)\<close>
+              hence \<open>k = 1\<close>
+                by simp
+              hence \<open>t = 2^l\<close>
+                using \<open>2^l * k = t\<close>
+                by auto
+              thus ?thesis
+                using \<open>t \<in> {2^l + 1 .. n}\<close>
+                by auto
+            qed
+            ultimately show ?thesis
+              by auto
+          qed
+          hence \<open>2^(Suc l) \<le> t\<close>
+            using \<open>2 ^ l * k = t\<close> 
+            by auto
+          hence \<open>2^(Suc l) \<le> n\<close>
+            using \<open>t \<in> {2^l + 1 .. n}\<close>
+            by auto
+          moreover have \<open>n < 2^(Suc l)\<close>
+          proof -
+            have f1: "\<forall>n na. (n \<le> na) = (int n + - 1 * int na \<le> 0)"
+              by auto
+            have f2: "int (Suc (nat \<lfloor>log 2 (real n)\<rfloor>)) + - 1 * int (Suc l) \<le> 0"
+              by (simp add: l_def)
+            have f3: "(- 1 * log 2 (real n) + real (Suc l) \<le> 0) = (0 \<le> log 2 (real n) + - 1 * real (Suc l))"
+              by fastforce
+            have f4: "real (Suc l) + - 1 * log 2 (real n) = - 1 * log 2 (real n) + real (Suc l)"
+              by auto
+            have f5: "\<forall>n na. \<not> 2 ^ n \<le> na \<or> real n + - 1 * log 2 (real na) \<le> 0"
+              by (simp add: le_log2_of_power)
+            have f6: "\<forall>x0 x1. (- 1 * int x0 + int (2 ^ x1) \<le> 0) = (0 \<le> int x0 + - 1 * int (2 ^ x1))"
+              by auto
+            have f7: "\<forall>x0 x1. int (2 ^ x1) + - 1 * int x0 = - 1 * int x0 + int (2 ^ x1)"
+              by auto
+            have "\<not> 0 \<le> log 2 (real n) + - 1 * real (Suc l)"
+              using f2 by linarith
+            then have "\<not> 0 \<le> int n + - 1 * int (2 ^ Suc l)"
+              using f7 f6 f5 f4 f3 f1 by (metis (no_types))
+            then show ?thesis
+              by linarith
+          qed                    
+          ultimately show ?thesis
+            by auto
+        qed
+        moreover have \<open>pnorm 2 (2 ^ l) \<ge> 0\<close>
+          using \<open>prime 2\<close> pnorm_geq_zero 
+          by blast
+        moreover have \<open>pnorm 2 (Fract 1 t) \<ge> 0\<close>
+          using \<open>prime 2\<close> pnorm_geq_zero 
+          by blast
+        ultimately show ?thesis 
+          by simp
+      qed
+      ultimately show ?thesis
+        by (smt Max_in)
+    next
+      case False
+      hence \<open>2 ^ l + 1 > n\<close>
+        by simp
+      hence \<open>{2 ^ l + 1..n} = {}\<close>
+        by simp
+      hence \<open>post_H = 0\<close>
+        unfolding post_H_def
+        by simp        
+      hence \<open>(2^l) * post_H = 0\<close>
+        by (simp add: \<open>post_H = 0\<close>)        
+      thus ?thesis
+        unfolding pnorm_def
+        by auto
+    qed
     ultimately have \<open>pnorm 2 (((2^l) *  (Fract 1 (of_nat (2^l))) 
                                   + (2^l) * pre_H) + ((2^l) * post_H)) = 1\<close>
       using pnorm_unit_ball[where p = 2 and x = "(2^l) *  (Fract 1 (of_nat (2^l))) + (2^l) * pre_H" 
@@ -382,26 +608,39 @@ proof-
       by auto
   qed
   hence \<open>pnorm 2 H = 2^l\<close>
-    by simp    
+    by simp
   thus ?thesis
-    using H_def l_def
-    by blast
+    using H_def l_def harmonic_explicit[where n = n]
+    by simp   
 qed
 
 
-subsection \<open>Main result\<close>
+subsection \<open>Main results\<close>
 
-theorem harmonic_numbers_are_not_integers:
+text\<open>The following result is due to L. Taeisinger ~\cite{theisinger1915bemerkung}.\<close>
+theorem Taeisinger:
   fixes n :: nat
   assumes \<open>n \<ge> 2\<close>
-  shows \<open>(\<Sum>k = 1..n. (Fract 1 (of_nat k)) ) \<notin> \<int>\<close>
+  shows \<open>harmonic n \<notin> \<int>\<close>
 proof-
   have \<open>pnorm 2 (\<Sum>k = 1..n. (Fract 1 (of_nat k)) ) > 1\<close>
-    using harmonic_numbers_2norm[where n = "n"]  \<open>n \<ge> 2\<close>
+    using harmonic_numbers_2norm[where n = "n"]  \<open>n \<ge> 2\<close> harmonic_explicit 
     by auto    
   thus ?thesis
-    by (smt integers_pnorm_D two_is_prime_nat)
+  proof -
+    have "\<not> pnorm 2 (\<Sum>n = 1..n. Fract 1 (int n)) \<le> 1"
+      using \<open>1 < pnorm 2 (\<Sum>k = 1..n. Fract 1 (int k))\<close> by linarith
+    then show ?thesis
+      by (metis (no_types) harmonic_explicit integers_pnorm_D two_is_prime_nat)
+  qed
 qed
+
+text\<open>The following result is due to J. K{\"u}rsch{\'a}k  ~\cite{kurschak1918harmonic}.\<close>
+theorem Kurschak:
+  fixes n m :: nat
+  assumes \<open>m + 2 \<le> n\<close>
+  shows \<open>harmonic n - harmonic m \<notin> \<int>\<close>
+  sorry
 
 end
 
